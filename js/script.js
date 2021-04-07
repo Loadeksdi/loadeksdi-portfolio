fa28346b84bc64f81d890307157819e9fd84e63f
Loadeksdi <nicolas.load@gmail.com>
2021-04-07 23:11:42 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function updateDocumentOnClick() {
+    document.getElementsByTagName("html").item(0).style.setProperty("background", "rgba(49, 49, 57, 1) no-repeat")
+    document.getElementsByTagName("button").item(0).style.setProperty("display","none");
+    document.getElementsByTagName("h1").item(0).style.setProperty("display","none");
+    document.getElementsByTagName("section").item(0).style.setProperty("display", "block");
+}

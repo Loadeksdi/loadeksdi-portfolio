fa861f22aa2c9d4caa174b9d34d17dc233fbacab
Loadeksdi <nicolas.load@gmail.com>
2021-04-08 23:09:35 +0200

    Dev: Added footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-function updateDocumentOnClick() {
-    document.getElementsByTagName("html").item(0).style.setProperty("background", "rgba(49, 49, 57, 1) no-repeat")
-    document.getElementsByClassName("content").item(0).style.setProperty("display","none");
-    document.getElementsByClassName("introduction").item(0).style.setProperty("display", "block");
-    document.getElementsByTagName("header").item(0).style.setProperty("display", "block");
-    document.getElementsByTagName("footer").item(0).style.setProperty("display", "block");
+function updateDocumentOnButtonClick() {
+    document.querySelector("html").style.background = "rgba(49, 49, 57, 1) no-repeat";
+    document.querySelector(".content").style.display = "none";
+    document.querySelector(".container").style.display = "flex";
+    document.querySelector(".introduction").style.display = "block";
+    document.querySelector(".top").style.display = "block";
+    document.querySelector("footer").style.display = "block";
+}
+
+function updateDocumentOnProClick(){
+    document.querySelector(".introduction").style.display = "none";
+    document.querySelector(".pro").style.display = "block";
 }

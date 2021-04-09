1b847a25142107504bfbfb661318b244b5f46793
Loadeksdi <nicolas.load@gmail.com>
2021-04-09 22:08:09 +0200

    Dev: Added content and features

--- js/script.js
+++ js/script.js
@@ -1,13 +1,59 @@
-function updateDocumentOnButtonClick() {
-    document.querySelector("html").style.background = "rgba(49, 49, 57, 1) no-repeat";
-    document.querySelector(".content").style.display = "none";
-    document.querySelector(".container").style.display = "flex";
-    document.querySelector(".introduction").style.display = "block";
-    document.querySelector(".top").style.display = "block";
-    document.querySelector("footer").style.display = "block";
+function updateDocumentBasedOnCaller(caller) {
+    switch (caller) {
+        case 0:
+            document.querySelector("html").style.background = "rgba(49, 49, 57, 1) no-repeat";
+            document.querySelector(".content").style.display = "none";
+            document.querySelector(".container").style.display = "flex";
+            document.querySelector(".introduction").style.display = "block";
+            document.querySelector(".top").style.display = "block";
+            document.querySelector("footer").style.display = "block";
+            break;
+        case 1:
+            document.querySelector(".introduction").style.display = "block";
+            document.querySelector(".pro").style.display = "none";
+            document.querySelector(".interests").style.display = "none";
+            document.querySelector(".projects").style.display = "none";
+            document.querySelector(".activity").style.display = "none";
+            break;
+        case 2:
+            document.querySelector(".introduction").style.display = "none";
+            document.querySelector(".pro").style.display = "block";
+            document.querySelector(".interests").style.display = "none";
+            document.querySelector(".projects").style.display = "none";
+            document.querySelector(".activity").style.display = "none";
+            break;
+        case 3:
+            document.querySelector(".introduction").style.display = "none";
+            document.querySelector(".pro").style.display = "none";
+            document.querySelector(".interests").style.display = "block";
+            document.querySelector(".projects").style.display = "none";
+            document.querySelector(".activity").style.display = "none";
+            break;
+        case 4:
+            document.querySelector(".introduction").style.display = "none";
+            document.querySelector(".pro").style.display = "none";
+            document.querySelector(".interests").style.display = "none";
+            document.querySelector(".projects").style.display = "block";
+            document.querySelector(".activity").style.display = "none";
+            break;
+        case 5:
+            document.querySelector(".introduction").style.display = "none";
+            document.querySelector(".pro").style.display = "none";
+            document.querySelector(".interests").style.display = "none";
+            document.querySelector(".projects").style.display = "none";
+            document.querySelector(".activity").style.display = "block";
+    }
 }
 
-function updateDocumentOnProClick(){
-    document.querySelector(".introduction").style.display = "none";
-    document.querySelector(".pro").style.display = "block";
+function sendMessage(){
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "212.47.231.250:3000", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    const message = JSON.stringify({message: {
+            time: new Date(),
+            text: document.querySelector(".message").value
+        }
+    });
+    console.log(message);
+    xhr.send(message);
 }

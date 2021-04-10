29dc5953780812b2b83965b7e53d973f540746cf
Loadeksdi <nicolas.load@gmail.com>
2021-04-10 22:47:08 +0200

    Dev: Changing URL

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let author;
-const socket = io("localhost:3000");
-//const socket = io("https://discord-bridge.loadeksdi.com:443");
+//const socket = io("localhost:3000");
+const socket = io("https://discord-bridge.loadeksdi.com:443");
 
 socket.on('message', async (msg) => {
    alert(msg.text);

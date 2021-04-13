75188953932bc64b749741896c5c107c92cefd74
Loadeksdi <nicolas.load@gmail.com>
2021-04-13 21:43:43 +0200

    Dev: Better UI for chat & interests

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 //const socket = io("localhost:3000");
 const socket = io("discord-bridge.loadeksdi.com");
 const messages = [];
+let first = true;
+let author;
 let id;
+const chat = document.querySelector(".chat");
+
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
@@ -22,14 +26,21 @@ function updateChat() {
     messages.forEach(msg => {
         str += `[${msg.date.toLocaleString(navigator.language)}] ${msg.author} : ${msg.text}\n`;
     });
-    document.querySelector(".chat").value = str;
+    chat.value = str;
 }
 
-document.querySelector("form").addEventListener('submit', function(e) {
+document.querySelector("form").addEventListener('submit', function (e) {
     e.preventDefault();
-    const nickname = document.querySelector(".nickname");
-    nickname.readOnly = true;
     const input = document.querySelector(".message");
+    if (first) {
+        author = input.value;
+        chat.value = `You logged in as ${input.value}!`;
+        input.placeholder = "Enter a message";
+        input.maxlength = 2000;
+        input.value = "";
+        first = false;
+        return;
+    }
     id = id === undefined ? uuidv4() : id;
     if (input.value) {
         const message = {
@@ -50,7 +61,7 @@ function updateDocumentBasedOnCaller(caller) {
         case 0:
             document.querySelector("html").style.background = "rgba(49, 49, 57, 1) no-repeat";
             document.querySelector(".content").style.display = "none";
-            document.querySelector(".container").style.display = "inline-flex";
+            document.querySelector(".container").style.display = "flex";
             document.querySelector(".introduction").style.display = "block";
             document.querySelector(".top").style.display = "block";
             document.querySelector("footer").style.display = "block";

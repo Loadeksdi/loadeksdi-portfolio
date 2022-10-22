733dd98bef3e8b70ce8341640a9922fdce8a2e4f
Loadeksdi <nicolas.load@gmail.com>
2022-10-22 03:53:25 +0200

    Setting up tailwind and project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex h-screen">
+	<div class="m-auto">
+		<h1 class="text-3xl font-bold underline">Welcome to SvelteKit!</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</div>
+</main>
